abca0d23b92357c50076efba867da6e28c09c44a
GonzaSalda <gonza.salda00@gmail.com>
2024-07-13 15:20:21 -0300

    Add phone, email, and social icons to the header

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,9 +4,12 @@ import { CursorContext } from "./CursorContext";
 import Link from "next/link";
 import Image from "next/image";
 import { AiOutlineMenu } from "react-icons/ai";
+import { FaPhoneAlt } from "react-icons/fa";
+import {IoMdMail} from "react-icons/io";
 import { motion } from "framer-motion";
 import MobileNav from "./MobileNav";
 import Nav from "./Nav";
+import Socials from "./Socials";
 
 const Header = () => {
   const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext);
@@ -19,7 +22,27 @@ const Header = () => {
     >
       {/* Topbar */}
       <div className="bg-secondary-100 mb-6 xl:mb-[50px] xl:h-[50px] py-4 xl:py-0">
-        topbar
+        <div className="container mx-auto h-full">
+          <div className=" flex items-center justify-between h-full">
+            {/* Phone & email */}
+            <div className="flex flex-col lg:flex-row items-center h-full gap-2 xl:gap-6 w-full justify-between xl:w-auto xl:justify-normal">
+              {/* Phone */}
+              <div className="flex gap-2 items-center text-white">
+                <FaPhoneAlt className="text-white" />
+                <span className="text-white">+123 456 7890</span>
+              </div>
+              {/* Email */}
+              <div className="flex gap-2 items-center text-white">
+                  <IoMdMail className="text-white" />
+                  <span className="text-white">info@gmail.com</span>
+              </div>
+            </div>
+            {/* Socials */}
+            <div className="hidden xl:block">
+              <Socials containerStyles="flex gap-6 text-white"/>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="container mx-auto flex items-center justify-between px-6">
         {/* Logo */}
@@ -47,13 +70,12 @@ const Header = () => {
           animate={{ right: mobileNav ? 0 : "-100%" }}
           className="fixed bg-primary top-0 bottom-0 right-0 w-[300px] xl:hidden z-50"
         >
-          <MobileNav setMobileNav={setMobileNav}/>
+          <MobileNav setMobileNav={setMobileNav} />
         </motion.div>
-      {/* Desktop nav */}
-      <div className="hidden xl:block">
-        <Nav/>
-      </div>
-
+        {/* Desktop nav */}
+        <div className="hidden xl:block">
+          <Nav />
+        </div>
       </div>
     </header>
   );
