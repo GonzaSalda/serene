d361d76e60e6c730c8178a5ff68dee92654a32ab
GonzaSalda <gonza.salda00@gmail.com>
2024-07-13 13:40:17 -0300

    Add mobile navigation and update page transition

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,10 +1,45 @@
+"use client";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { IoCloseOutline } from "react-icons/io5";
+
+const links = [
+  {
+    href: "/",
+    name: "Home",
+  },
+  {
+    href: "/about",
+    name: "About",
+  },
+  {
+    href: "/treatments",
+    name: "Treatments",
+  },
+  {
+    href: "/contact",
+    name: "Contact",
+  },
+];
+
 const MobileNav = ({ setMobileNav }) => {
+  const pathname = usePathname();
   return (
-    <nav className="text-white">
-      <div className="cursor-pointer" onClick={() => setMobileNav(false)}>
+    <nav className="relative flex flex-col justify-between h-full p-8">
+      <div className="cursor-pointer text-accent" onClick={() => setMobileNav(false)}>
         <IoCloseOutline className="text-4xl" />
       </div>
+      <ul className=" flex flex-col gap-10 text-white text-xl">
+        {links.map((link, index) => {
+          return (
+            <Link href={link.href} key={index} className={`${pathname === link.href && "border-b-2 border-accent"} uppercase max-w-max mx-auto`}>
+              {link.name}
+            </Link>
+          );
+        })}
+      </ul>
+      <div className="text-white">socials</div>
     </nav>
   );
 };
