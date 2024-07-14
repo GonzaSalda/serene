9fc71273ebd93deff9d063ce300d6d85ea01f2c5
GonzaSalda <gonza.salda00@gmail.com>
2024-07-13 23:38:20 -0300

    Update landing page layout and content

--- app/page.jsx
+++ app/page.jsx
@@ -11,14 +11,35 @@ const Home = () => {
       animate={{ opacity: 1, transition: { delay: 2 } }}
       className="min-h-screen flex items-center overflow-x-hidden"
     >
-      <div className="container  mx-auto"> 
+      <div className="container  mx-auto">
         <div className="flex flex-col xl:flex-row items-center h-full">
           {/* text */}
-          <div>text</div>
+          <div className="w-full text-center xl:text-left xl:w-[500px] pt-[120px]">
+            <h1 className="h1">
+              Natural Beauty <br />
+              Starts here
+            </h1>
+            <p className="lead max-w-xl mx-auto">
+              Tailored skincare solutions for a healthy complexion, offering
+              customized care for radiant skin
+            </p>
+            <div>
+              <button className="btn btn-lg">Book an appointment</button>
+              <div>
+                <ModalVideo/>
+              </div>
+            </div>
+          </div>
           {/* Image */}
           <div className="flex-1">
             <div className="hidden xl:flex fixed bottom-0">
-              <Image src={"/"} />
+              <Image
+                src={"/assets/home/img.png"}
+                width={864}
+                height={650}
+                quality={100}
+                alt=""
+              />
             </div>
           </div>
         </div>
